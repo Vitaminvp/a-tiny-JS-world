9effa99dbdc7c4158ac2a66036f85983e5919396
Vitalii Ovcharenko <vitamin@ukr.net>
2018-12-28 10:20:33 +0200

    Add post OOP tiny JS world

--- index.js
+++ index.js
@@ -7,7 +7,49 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+class Creature {
+	constructor(species,  gender, name, legs, hands, saying) {
+		this.species = species;
+		this.legs    = legs;
+		this.hands   = hands;
+		this.name    = name;
+		this.gender  = gender;
+		this.saying  = saying;
+		this.friends = [];
+	}
+    addFriends(...args){
+        this.friends = [...args];
+    }
+	printCreature(){
+		return ['species', 'gender', 'name', 'legs', 'hands', 'saying']
+				  .map(key => this[key])
+				  .concat(this.friends.map(friend => friend['name']))
+				  .join('; ');
+	}
+}
 
+class SuperHero extends Creature {
+    constructor(prototipe1, prototipe2, name, legs, hands) {
+      super(null, null, name, legs, hands);
+	  this.species = prototipe1.species + '-' + prototipe2.species;
+	  this.gender  = prototipe1.gender;
+      this.saying  = prototipe2.saying;
+    }
+}
+
+const woman    = new Creature('human', 'female', 'Sarah', 2, 2, 'Love saves the world.');
+const man      = new Creature('human', 'male', 'John', 2, 2, 'Boys will be boys.');
+const cat      = new Creature('cat', 'female', 'Maya', 4, 0, 'Meow.');
+const dog      = new Creature('dog', 'male', 'Toby', 4, 0, 'Woof-woof!');
+const catWoman = new SuperHero(woman, cat, 'Sophie', 2, 2);
+const dogMan   = new SuperHero(man, dog, 'Barsik', 2, 2);
+
+
+woman.addFriends(man, cat);
+man.addFriends(woman, dog);
+dog.addFriends(woman, man);
+catWoman.addFriends(cat, dog);
+dogMan.addFriends(cat, dog, woman, man);
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -17,6 +59,7 @@
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.
    */
+   [woman, man, cat, dog, catWoman, dogMan].map(creature => print(creature.printCreature()));
 
 /* Print examples:
    print('ABC');
